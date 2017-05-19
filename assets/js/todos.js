fe7cca0881edeededb2b492869f76c3771d9c988
Kelvin Jose <kelvinkonoor@gmail.com>
2017-05-19 09:17:59 +0530

    A small to do app

---
+++ assets/js/todos.js
@@ -0,0 +1,22 @@
+ $('ul').on("click","li",function() {
+   $(this).toggleClass("checked");
+ })
+
+ $('ul').on('click','span',function(event) {
+   $(this).parent().fadeOut(500,function() {
+     $(this).remove();
+   });
+   event.stopPropogation();
+ })
+
+ $('input[type="text"]').on('keypress',function(event) {
+   if(event.which === 13) {
+   var todo = $(this).val();
+   $(this).val("");
+   $('ul').append("<li><span><i class='fa fa-trash'></i> </span>"+todo+"</li>")
+ }
+ })
+
+$('.fa-plus').click(function() {
+  $('input').fadeToggle();
+})
